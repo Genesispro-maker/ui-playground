b37fd4351d416f371fab9028c0a2315a6bd5b36e
Genesispro-maker <genesisprog894@gmail.com>
2026-01-08 13:31:54 -0800

    added shadows and made the inputs interactive

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,8 +1,10 @@
 import { Header } from "./components/header";
+import { Illustration } from "./components/Illustration";
 function App(){
    return (
     <>
-      <Header />
+     <Header />
+     <Illustration />
     </>
    )
 }
--- ui/src/components/Illustration.tsx
+++ ui/src/components/Illustration.tsx
@@ -1,16 +1,22 @@
 import { useState } from "react"
+import styles from "../styles/illustration.module.css"
 
 export function Illustration(){
     const [horizontalPosition, setHorizontalPosition] = useState<number>(0)
     const [verticalPosition, setVerticalPosition] = useState<number>(0)
 
+    const translateX = horizontalPosition
+    const translateY = verticalPosition
+
     return(
         <>
-          <div></div>
+        <div className={styles.boxWrapper}>
+          <div className={styles.box} style={{translate: `${translateX}px ${translateY}px`}}></div>
+          </div>
 
-          <div>
+          <div className={styles.wrapper}>
             <input type="range" min={-100} max={100} value={horizontalPosition} onChange={(e) => setHorizontalPosition(Number(e.target.value))}/>
-            <input type="range" min={-100} max={100} value={verticalPosition} onChange={(e) => setVerticalPosition(Number(e.target.value))}/>
+            <input type="range" className={styles.verticalInput} min={-100} max={100} value={verticalPosition} onChange={(e) => setVerticalPosition(Number(e.target.value))}/>
           </div>
         </>
     )
