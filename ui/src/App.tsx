dd77d5921b6a7161b5a79a22ef8d7b507427c2e9
Genesispro-maker <genesisprog894@gmail.com>
2026-01-19 11:06:46 -0800

    Update App.tsx

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,14 +1,15 @@
 import { AudioVisualizer } from "./components/audio-visualizer";
-// import { Charts } from "./components/charts";
+
 import { Header } from "./components/header";
 import { Illustration } from "./components/Illustration";
+import { WORDLE } from "./components/wordle-game/wordle-game";
 function App(){
    return (
     <>
      <Header />
      <Illustration />
      <AudioVisualizer />
-       {/* <Charts /> */}
+     <WORDLE />
     </>
    )
 }
