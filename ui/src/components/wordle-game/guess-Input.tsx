a105b0a53d8a2ffdbe099a00af65d9dfade4d896
Genesispro-maker <genesisprog894@gmail.com>
2026-01-19 11:24:03 -0800

    Update guess-Input.tsx

--- ui/src/components/wordle-game/guess-Input.tsx
+++ ui/src/components/wordle-game/guess-Input.tsx
@@ -1,18 +1,18 @@
 import { useState, type ChangeEvent } from "react"
 
-export function GuessInput(){
-      const [guess, setGuess] = useState("")
+export function GuessInput({handleGuessInput}){
+      const [tentativeGuess, setTentativeGuess] = useState("")
 
     function handleSubmit(e: ChangeEvent<HTMLFormElement>){
         e.preventDefault()
-        console.log(guess)
-        setGuess('')
+         handleGuessInput(tentativeGuess)
+         setTentativeGuess('')
     }
     return (
         <>
           <form onSubmit={handleSubmit}>
             <label htmlFor="Guess">Enter Guess : </label>
-            <input type="text" required minLength={5} maxLength={5} value={guess} onChange={(e) => setGuess(e.target.value.toUpperCase())}/>
+            <input type="text" required minLength={5} maxLength={5} value={tentativeGuess} onChange={(e) => setTentativeGuess(e.target.value.toUpperCase())}/>
           </form>
         </>
     )
