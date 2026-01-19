0a69c988b8604a1961d9f7943fbea89385757ff6
Genesispro-maker <genesisprog894@gmail.com>
2026-01-19 12:09:55 -0800

    Update guess.tsx

--- ui/src/components/wordle-game/guess.tsx
+++ ui/src/components/wordle-game/guess.tsx
@@ -1,14 +1,16 @@
 import styles from "./wordle.module.css"
 import { range } from "../../utils/game"
 
-export function Guess({value}: {value: string}){
-    return (
-        <>
-          <div>
-            <p className={styles.letter}>{range(5).map((num) => {
-               return <span className={styles.cell} key={num}>{value ? value[num] : undefined}</span>
-            })}</p>
-          </div>
-        </>
-    )
+export function Guess({ value }: { value?: string }) {
+  return (
+    <div>
+      <p className={styles.letter}>
+        {range(5).map((num) => (
+          <span className={styles.cell} key={num}>
+            {value ? value[num] : null}
+          </span>
+        ))}
+      </p>
+    </div>
+  )
 }
