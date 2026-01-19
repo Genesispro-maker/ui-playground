5503b50c61c5819de49d1401760ed6317540ffc6
Genesispro-maker <genesisprog894@gmail.com>
2026-01-19 11:06:35 -0800

    Update wordle-game.tsx

--- ui/src/components/wordle-game/wordle-game.tsx
+++ ui/src/components/wordle-game/wordle-game.tsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import { SAMPLE, WORDS } from "../../utils/game";
-import { GuessInput } from "./GuessInput";
+import { GuessInput } from "./guess-Input";
+import { GuessResult } from "./guess-result";
 import { Header } from "./header";
 
 const answer = SAMPLE(WORDS)
 console.log({answer})
 
 export function WORDLE(){
+    const [guess, setGuess] = useState("")
     return (
         <>
           <Header />
-          <GuessInput />
+          <GuessResult />
+          <GuessInput guess={guess} setGuess={setGuess}/>
         </>
     )
 }
