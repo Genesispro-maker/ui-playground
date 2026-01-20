0ddae7e5500aa148e7d0be3eca881a7fd3de82bc
Genesispro-maker <genesisprog894@gmail.com>
2026-01-20 04:37:21 -0800

    Update guess-Input.tsx

--- ui/src/components/wordle-game/guess-Input.tsx
+++ ui/src/components/wordle-game/guess-Input.tsx
@@ -1,5 +1,5 @@
 import { useState, type ChangeEvent } from "react"
-
+import styles from "./wordle.module.css"
 export function GuessInput({handleGuessInput, gameStatus}: {handleGuessInput: any, gameStatus: string}){
       const [tentativeGuess, setTentativeGuess] = useState("")
 
@@ -13,8 +13,10 @@ export function GuessInput({handleGuessInput, gameStatus}: {handleGuessInput: an
     return (
         <>
           <form onSubmit={handleSubmit}>
-            <label htmlFor="Guess">Enter Guess : </label>
-            <input disabled={gameStatus !== "running"} type="text" required minLength={5} maxLength={5} value={tentativeGuess} onChange={(e) => setTentativeGuess(e.target.value.toUpperCase())}/>
+            <div className={styles.formWrapper}>
+            <label className={styles.label} htmlFor="Guess">Enter Guess : </label>
+            <input className={styles.gameInput} disabled={gameStatus !== "running"} type="text" required minLength={5} maxLength={5} value={tentativeGuess} onChange={(e) => setTentativeGuess(e.target.value.toUpperCase())}/>
+            </div>
           </form>
         </>
     )
