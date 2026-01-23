d0325fea910e6f145e112a649dc2371beb45d7c7
Genesispro-maker <genesisprog894@gmail.com>
2026-01-23 13:48:31 -0800

    Update guess-result.tsx

--- ui/src/components/wordle-game/guess-result.tsx
+++ ui/src/components/wordle-game/guess-result.tsx
@@ -4,9 +4,10 @@ import { range, NUM_OF_GUESSES } from "../../utils/game"
 export function GuessResult({ guesses, answer }: { guesses: string[], answer: string }) {
   return (
     <div>
-      {range(NUM_OF_GUESSES).map((num) => (
-        <Guess key={num} value={guesses[num]} answer={answer}/>
-      ))}
+      {range(NUM_OF_GUESSES).map((num) => {
+        console.log(num)
+       return <Guess key={num} value={guesses[num]} answer={answer}/>
+})}
     </div>
   )
 }
