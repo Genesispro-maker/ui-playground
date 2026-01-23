5e1e5998e9836f5ffc2fa8adea17631bad13836d
Genesispro-maker <genesisprog894@gmail.com>
2026-01-23 13:48:34 -0800

    Update guess.tsx

--- ui/src/components/wordle-game/guess.tsx
+++ ui/src/components/wordle-game/guess.tsx
@@ -7,11 +7,12 @@ export function Guess({ value}: { value: string, answer: string}) {
   return (
     <div>
       <p className={styles.letter}>
-        {range(5).map((num) => (
-          <span className={styles.cell} key={num}>
+        {range(5).map((num) => {
+          console.log(num)
+         return <span className={styles.cell} key={num}>
             {value ? value[num] : null}
           </span>
-        ))}
+        })}
       </p>
     </div>
   )
