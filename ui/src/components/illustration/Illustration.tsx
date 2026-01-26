7ed29b4fca30d98b384edb6188d818809c7eb92c
Genesispro-maker <genesisprog894@gmail.com>
2026-01-26 04:13:20 -0800

    update

--- ui/src/components/illustration/Illustration.tsx
+++ ui/src/components/illustration/Illustration.tsx
@@ -1,33 +1,40 @@
 import { useState } from "react"
-import styles from "../styles/illustration.module.css"
+import styles from "./illustration.module.css"
 
 export function Illustration(){
-     const [position, setPosition] = useState({
-        x: 0,
-        y: 0,
-     })
+     const [position, setPosition] = useState({x: 0, y: 0})
+
     return(
         <>
         <main className={styles.boxWrapper}>
-          <div className={styles.boxWrap}>
+
+          <div className={styles.BoxWrap}>
+
           <div className={styles.box} style={{translate: `${position.x}px ${position.y}px`}}></div>
+
           </div>
 
               <div className={styles.valuesWrapper}>
+
                 <code>transform: translate({position.x}px, {position.y}px)</code>
+
               </div>
+
           <div className={styles.Rangewrapper}>
-            <p className={styles.Text}>X : {position.x}</p>
-            <input type="range" className={styles.verticalRange} min={-100} max={100} value={position.x} onChange={(e) => {
+
+            <p className={styles.value} role="verticalValue">X : {position.x}</p>
+
+            <input type="range" role="verticalInputcontrol" className={styles.input} min={-100} max={100} value={position.x} onChange={(e) => {
               const newPosition =  {...position, x: Number(e.target.value)}
               setPosition(newPosition)
             }}/>
              
-            <input type="range" min={-100} max={100} value={position.y} onChange={(e) => {
+            <input className={styles.input} role="horizontalInputcontrol" type="range" min={-100} max={100} value={position.y} onChange={(e) => {
               const newPosition = {...position, y: Number(e.target.value)}
               setPosition(newPosition)
             }}/>
-            <p className={styles.Text}>Y: {position.y}</p>
+
+            <p className={styles.value} role="horizontalValue">Y: {position.y}</p>
 
           </div>
           </main>
---
+++ ui/src/components/passport/types.ts
@@ -0,0 +1,1 @@
+export type PassportType = Array<string>
