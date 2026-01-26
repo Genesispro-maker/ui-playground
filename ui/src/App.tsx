b7d32f58b0892a6d3f2d06e592b3e6b119f84fcc
Genesispro-maker <genesisprog894@gmail.com>
2026-01-26 05:48:42 -0800

    Update App.tsx

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,22 @@
 import { AudioVisualizer } from "./components/apple-music-player/audio-visualizer";
-
+import { useMorph } from "./utils/morph";
 import { Header } from "./components/header";
 import { Illustration } from "./components/illustration/Illustration";
 import { WebPassport } from "./components/passport/web-passport";
 
+
+const Textdata: Array<string> = ["turf", "Nuclear-Testsite", "Artillery", "Den"]
+
+
 function App(){
+   const morph = useMorph(Textdata)
+   
    return (
     <>
      <Header />
+     <div>
+        <h3>welcome to my {Textdata[morph]}</h3>
+     </div>
      <Illustration />
      <AudioVisualizer />
      <WebPassport />
