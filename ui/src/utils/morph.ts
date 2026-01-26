d611368b658061bee2496064b6717c4d3309ece1
Genesispro-maker <genesisprog894@gmail.com>
2026-01-26 13:23:45 -0800

    Update morph.ts

--- ui/src/utils/morph.ts
+++ ui/src/utils/morph.ts
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
+import { type TextDataType } from "./data"
 
-export const useMorph = (TextData: Array<string>) => {
+
+export const useMorph = (TextData: Array<TextDataType>) => {
      const [index, setIndex] = useState(0)
     useEffect(() => {
         const textMorphInterval = window.setInterval(() => {
