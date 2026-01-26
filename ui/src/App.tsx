0ae54918923e52ad0a71d826c7f353d2970ff0d6
Genesispro-maker <genesisprog894@gmail.com>
2026-01-26 13:23:39 -0800

    Update App.tsx

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,19 +3,17 @@ import { useMorph } from "./utils/morph";
 import { Header } from "./components/header/header";
 import { Illustration } from "./components/illustration/Illustration";
 import { WebPassport } from "./components/passport/web-passport";
-
-
-const Textdata: Array<string> = ["turf", "Nuclear-Testsite", "Artillery", "Den"]
+import { TextData } from "./utils/data";
 
 
 function App(){
-   const morph = useMorph(Textdata)
+   const morph = useMorph(TextData)
    
    return (
     <>
      <Header />
      <div className="herotext">
-        <h3>welcome to my <span>{Textdata[morph]}</span></h3>
+        <h3>welcome to my <span style={{color: TextData[morph].color}}><img src={TextData[morph].icon} alt={TextData[morph].text} width={100} height={100}/> {TextData[morph].text}</span> where i put my <br /> UI engineering skills to work</h3>
      </div>
      <Illustration />
      <AudioVisualizer />
