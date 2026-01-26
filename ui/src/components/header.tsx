8e1bbe8b98f8ff06d670a8d983b0bb84c3c9669d
Genesispro-maker <genesisprog894@gmail.com>
2026-01-26 04:12:39 -0800

    Update header.tsx

--- ui/src/components/header.tsx
+++ ui/src/components/header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <Fragment>
          <header className={styles.header}>
-            <h2>UI PLAY</h2>
+            <h2 style={{color: "white"}}>UI PLAY</h2>
 
               <a href="https://github.com/Genesispro-maker/" className={styles.link}>
                  <Github size={25} fill="none" className={styles.github}/>
