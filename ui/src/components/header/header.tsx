8f7c7b872e201b2071047e5b9a94709b4cf092c1
Genesispro-maker <genesisprog894@gmail.com>
2026-01-29 13:07:37 -0800

    theme support

--- ui/src/components/header/header.tsx
+++ ui/src/components/header/header.tsx
@@ -1,17 +1,40 @@
-import { Github } from "lucide-react"
+import { Github, Moon, Sun } from "lucide-react"
 import { Fragment } from "react/jsx-runtime"
 import styles from "./header.module.css"
+import { useState } from "react"
+
 
 
 
 export const Header = () => {
+  const [theme, setThemeState] = useState("dark")
+  
+function setTheme(theme: "light" | "dark"){
+  document.body.setAttribute("data-theme", theme)
+
+  setThemeState(theme)
+
+
+  localStorage.setItem("theme", theme)
+}
+
+
+
+function toggleTheme(){
+   const newTheme = theme === "light" ? "dark" : "light"
+   setTheme(newTheme)
+}
+
   return (
     <Fragment>
          <header className={styles.header}>
             <h2 className={styles.heading}>UI PLAY</h2>
+            <div className={styles.themeButtonWrapper}>
               <a href="https://github.com/Genesispro-maker/" className={styles.link} role="link" aria-label="link">
                  <Github size={25} fill="none" className={styles.github}/>
               </a> 
+                <button className={styles.themeButton} onClick={toggleTheme}>{theme === "light" ? <Moon className={styles.themeIcon}/> : <Sun className={styles.themeIcon}/>}</button>
+              </div>
          </header>
     </Fragment>
   )
