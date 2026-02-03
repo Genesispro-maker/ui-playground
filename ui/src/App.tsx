dfd3777e4cb9d7b04b40aea2e6956f62049e4e9f
Genesispro-maker <genesisprog894@gmail.com>
2026-02-03 05:52:10 -0800

    wip port:80

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,13 +2,31 @@ import { AudioVisualizer } from "./components/audio-visualizer";
 import { Drawer } from "./components/drawer";
 import { Header } from "./components/header";
 import { Illustration } from "./components/Illustration";
+import { useState, useCallback } from "react";
+
+
+
+function useToggle(initialValue = false){
+    const [toggle, setToggle] = useState(initialValue)
+
+
+    const handleToggleOpen = useCallback(() => {
+        setToggle((currentToggle) => !currentToggle)
+    },  [])
+
+    return [toggle, handleToggleOpen]
+}
+
 function App(){
+   const [isOpen, handleIsOpen] = useToggle(false)
    return (
     <>
      <Header />
      <Illustration />
      <AudioVisualizer />
-     <Drawer initialValue={0}/>
+     {isOpen && <Drawer initialValue={0}/>}
+
+     <button onClick={handleIsOpen}>{isOpen ? "close drawer" : "open drawer"}</button>
     </>
    )
 }
