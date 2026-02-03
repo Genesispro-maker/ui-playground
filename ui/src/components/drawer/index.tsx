f6b4c83e9638a2249fb8e5e99f360c389be69ec0
Genesispro-maker <genesisprog894@gmail.com>
2026-02-03 11:32:32 -0800

    Update index.tsx

--- ui/src/components/drawer/index.tsx
+++ ui/src/components/drawer/index.tsx
@@ -1,6 +1,8 @@
 import { Minus, Plus } from "lucide-react";
 import { useState } from "react";
 import styles from './drawer.module.css'
+import { createPortal } from "react-dom";
+
 
 export const Drawer = ({initialValue} : {initialValue: number}) => {
    const [Amount, setAmount] = useState(initialValue)
@@ -19,14 +21,14 @@ export const Drawer = ({initialValue} : {initialValue: number}) => {
         setAmount((amount) => amount - 1)
     }
 
-   return(
-      <div className={styles.wrapper}>
-            <h1>{formattedPrice}</h1>
-
-            <div>
-                <button onClick={Increment}><Plus /></button>
-                <button onClick={Decrement}><Minus /></button>
-            </div>
+   return createPortal(
+       <div className={styles.wrapper}>
+      <div className={styles.drawer}>
+        <div className={styles.random}></div>
+          <h1>{formattedPrice}</h1>
+          <button onClick={Increment}><Plus /></button>
+          <button onClick={Decrement}><Minus /></button>
       </div>
+    </div>, document.querySelector("#drawer")!
 )
 }
