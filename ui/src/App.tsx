1579106914850e1fd46bb55dda2cffe5ffae44df
Genesispro-maker <genesisprog894@gmail.com>
2026-02-03 11:32:26 -0800

    Update App.tsx

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
-import { AudioVisualizer } from "./components/audio-visualizer";
+import { ApplePlayer } from "./components/apple-music-player/apple.music.player";
+import { useMorph } from "./utils/morph";
+import { Header } from "./components/header/header";
+import { Illustration } from "./components/illustration/Illustration";
+import { TextData } from "./utils/data"
 import { Drawer } from "./components/drawer";
-import { Header } from "./components/header";
-import { Illustration } from "./components/Illustration";
-import { useState, useCallback } from "react";
-
-
+import { useCallback, useState, useEffect } from "react";
 
 function useToggle(initialValue = false){
     const [toggle, setToggle] = useState(initialValue)
@@ -18,20 +17,26 @@ function useToggle(initialValue = false){
     return [toggle, handleToggleOpen]
 }
 
+
 function App(){
    const [isOpen, handleIsOpen] = useToggle(false)
-=======
-import { ApplePlayer } from "./components/apple-music-player/apple.music.player";
-import { useMorph } from "./utils/morph";
-import { Header } from "./components/header/header";
-import { Illustration } from "./components/illustration/Illustration";
-import { TextData } from "./utils/data";
-
-
-function App(){
    const morph = useMorph(TextData)
+
    
->>>>>>> main
+ useEffect(() => {
+       function Escape(e: KeyboardEvent){
+         if(e.code === "Escape"){
+            handleIsOpen()
+         }
+       }
+
+       window.addEventListener("keydown", Escape)
+
+       return () => {
+         window.addEventListener("keydown", Escape)
+       }
+    }, [isOpen, handleIsOpen])
+
    return (
     <>
      <Header />
@@ -39,13 +44,9 @@ function App(){
         <h3>welcome to my <span style={{color: TextData[morph].color, transform: "scale(1)", transition: "transform 0.5s ease-in-out"}}>{TextData[morph].text}</span> where i put my UI engineering skills to work</h3>
      </div>
      <Illustration />
-<<<<<<< HEAD
-     <AudioVisualizer />
+     <ApplePlayer />
      <button onClick={handleIsOpen}>{isOpen ? "close drawer" : "open drawer"}</button>
      {isOpen && <Drawer initialValue={0}/>}
-=======
-     <ApplePlayer />
->>>>>>> main
     </>
    )
 }
