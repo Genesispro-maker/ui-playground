f2594563fc6ebcce2dee8fa644c3bd5c33128d1c
Genesispro-maker <genesisprog894@gmail.com>
2026-02-04 03:35:19 -0800

    Update index.tsx

--- ui/src/components/drawer/index.tsx
+++ ui/src/components/drawer/index.tsx
@@ -1,51 +1,22 @@
-import { Minus, Plus } from "lucide-react";
-import { useRef, useState} from "react";
+import {type ReactNode} from "react";
 import styles from './drawer.module.css'
 import { createPortal } from "react-dom";
 import { X } from "lucide-react";
 
 
-export const Drawer = ({initialValue, handleClose} : {initialValue: number, handleClose: () => void}) => {
-   const [Amount, setAmount] = useState<number>(initialValue)
-
-   const wrapperRef = useRef<HTMLDivElement | null>(null)
-
-    const formattedPrice = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-    }).format(Amount)
-
-
-    const Increment = () => {
-        setAmount((amount) => amount + 1)
-    }
-
-    const Decrement = () => {
-        setAmount((amount) => amount - 1)
-    }
-
+export const Drawer = ({handleClose, children} : {handleClose: () => void, children: ReactNode}) => {
 
     function handleCloseDrawer(){
         handleClose()
     }
    
    return createPortal(
-       <div ref={wrapperRef} className={`${styles.wrapper}`}>
+       <div className={`${styles.wrapper}`}>
 
       <div className={styles.drawer}>
 
       <div className={styles.random} />
-
-      <h1 className={styles.heading}>{formattedPrice}</h1>
-
-      <div className={`${styles.buttonWrapper}`}>
-
-          <button className={styles.btns} onClick={Increment}><Plus /></button>
-
-          <button className={styles.btns} onClick={Decrement}><Minus /></button>
-
-          </div>
-
+             {children}
           <button className={styles.closeButton} onClick={() => handleCloseDrawer()}><X />close</button>
 
         </div>
