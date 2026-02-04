dfb5e42a6b85f311e3dca273aa86cc619efcebaf
Genesispro-maker <genesisprog894@gmail.com>
2026-02-04 06:25:24 -0800

    feat(created the Button Element)

--- ui/src/components/drawer/index.tsx
+++ ui/src/components/drawer/index.tsx
@@ -2,6 +2,7 @@ import {type ReactNode} from "react";
 import styles from './drawer.module.css'
 import { createPortal } from "react-dom";
 import { X } from "lucide-react";
+import { Button } from "./button";
 
 
 export const Drawer = ({handleClose, children} : {handleClose: () => void, children: ReactNode}) => {
@@ -17,8 +18,7 @@ export const Drawer = ({handleClose, children} : {handleClose: () => void, child
 
       <div className={styles.random} />
              {children}
-          <button className={styles.closeButton} onClick={() => handleCloseDrawer()}><X />close</button>
-
+          <Button className={styles.closeButton} onClick={() => handleCloseDrawer()}><X /> close</Button>
         </div>
     </div>, document.querySelector("#drawer")!
 )
